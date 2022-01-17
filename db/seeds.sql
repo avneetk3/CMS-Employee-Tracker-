4dd0fdf25a3f01cb48da4853d07d34089d66e2b4
avneetk3 <92407474+avneetk3@users.noreply.github.com>
2022-01-16 23:48:40 -0500

    Update seeds.sql
    Added CEO to seeds and corrected order of table input

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,3 @@
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Avneet', 'Kaur', 1, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Gurpreet', 'Kaur', 2, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Jasmeet', 'Singh', 3, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Ritika', 'Sharma', 3, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Kevin', 'Liu', 3, 2);
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Rochelle', 'Smith', 4, 2);
-
-
 INSERT INTO department (d_name)
 VALUES ('Management');
 INSERT INTO department (d_name)
@@ -31,3 +17,20 @@ INSERT INTO role (title, salary, department_id)
 VALUES ('HR Person', 150000, 4);
 INSERT INTO role (title, salary, department_id)
 VALUES ('CEO', 500000, null);
+
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Avneet', 'Kaur', 1, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Amarjit', 'Singh', 5, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Gurpreet', 'Kaur', 2, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Jasmeet', 'Singh', 3, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Ritika', 'Sharma', 3, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Kevin', 'Liu', 3, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Rochelle', 'Smith', 4, 2);
